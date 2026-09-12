9fa45451786c3500ae2accabaf8bbf1df2c95244
saniaMalik2005 <saniamalik0241@gmail.com>
2026-09-12 18:51:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Dev Stack</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h2>Dev Stack</h2>
+      </div>
+
+      <div>
+        <a href="#">Home</a>
+        <a href="#">Technologies</a>
+        <a href="#">Projects</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+
+      <div>
+        <a href="#">Sign In</a>
+        <button>Sign Up</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
