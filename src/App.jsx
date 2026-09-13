510edc2633b9cf32a82cb614f680f49036e855ea
saniaMalik2005 <saniamalik0241@gmail.com>
2026-09-13 21:07:03 +0600

    style: refine visual components and layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
 import Footer from "./components/Footer";
-
+//function 
 function App() {
   return (
     <>
